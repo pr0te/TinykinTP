370eb037f53871b2e935b34b0e2603f429473deb
pr0te <prote1337@gmail.com>
2022-09-22 22:39:05 +0200

    Credit added

--- Tinykin TP/Program.cs
+++ Tinykin TP/Program.cs
@@ -1,10 +1,12 @@
+/*
+ *  Hacked together in a hurry by pr0te
+ *  22-09-22
+ */
+
 global using Memory;
 
 namespace Tinykin_TP {
 	internal static class Program {
-		/// <summary>
-		///  The main entry point for the application.
-		/// </summary>
 		[STAThread]
 		static void Main() {
 			ApplicationConfiguration.Initialize();
